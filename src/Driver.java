1845659cbb110807fab22a257053dd87d3af92d5
Jason Riha <superjar30@gmail.com>
2020-09-30 11:54:21 -0500

    worked on assignment

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Driver
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		System.out.println("Hello World");
+		BibleBook[] ar = new BibleBook[27];
+		Scanner input = new Scanner(new File(System.getProperty("user.dir") + "/src/input.dat"));
+		while(input.hasNext())
+		{
+			BibleBook a = new BibleBook(input.nextLine());
+			a.display();
+		}
+		input.close();
 	}
 }
